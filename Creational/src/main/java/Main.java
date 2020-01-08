a6d23b46a4b484e9ab7381881c50f9ad6acf9476
Fernando Luiz Goulart <fernando.goulart@olxbr.com>
2020-01-08 13:09:02 -0300

    ajutes

--- Creational/src/main/java/Main.java
+++ Creational/src/main/java/Main.java
@@ -27,33 +27,11 @@ public class Main {
 
     }
 
-    public static Maze abstractFactoryCreateMaze(MazeFactory factory) {
-        Maze aMaze = factory.makeMaze();
-        Room r1 = factory.makeRoom(1);
-        Room r2 = factory.makeRoom(2);
-        Door aDoor = factory.makeDoor(r1, r2);
-
-        aMaze.addRoom(r1);
-        aMaze.addRoom(r2);
-
-        r1.setSide(Direction.NORTH, factory.makeWall());
-        r1.setSide(Direction.EAST, aDoor);
-        r1.setSide(Direction.SOUTH, factory.makeWall());
-        r1.setSide(Direction.WEST, factory.makeWall());
-
-        r2.setSide(Direction.NORTH, factory.makeWall());
-        r2.setSide(Direction.EAST, factory.makeWall());
-        r2.setSide(Direction.SOUTH, factory.makeWall());
-        r2.setSide(Direction.WEST, aDoor);
-
-        return aMaze;
-    }
-
     public static void main(String args[]) {
         //  Maze myMaze = createMaze();
         //  System.out.println(myMaze.getRoom(1).roomNumber);
 
-        Maze myMaze = abstractFactoryCreateMaze(new MazeFactory());
+        Maze myMaze = new MazeGame().createMaze(new MazeFactory());
         System.out.println(myMaze.getRoom(1).roomNumber);
 
         // Desafio: reutilizar um layout existente para um novo jogo com enchanted mazes
---
+++ Creational/src/main/java/MazeGame.java
@@ -0,0 +1,24 @@
+public class MazeGame {
+
+    public Maze createMaze(MazeFactory factory) {
+        Maze aMaze = factory.makeMaze();
+        Room r1 = factory.makeRoom(1);
+        Room r2 = factory.makeRoom(2);
+        Door aDoor = factory.makeDoor(r1, r2);
+
+        aMaze.addRoom(r1);
+        aMaze.addRoom(r2);
+
+        r1.setSide(Direction.NORTH, factory.makeWall());
+        r1.setSide(Direction.EAST, aDoor);
+        r1.setSide(Direction.SOUTH, factory.makeWall());
+        r1.setSide(Direction.WEST, factory.makeWall());
+
+        r2.setSide(Direction.NORTH, factory.makeWall());
+        r2.setSide(Direction.EAST, factory.makeWall());
+        r2.setSide(Direction.SOUTH, factory.makeWall());
+        r2.setSide(Direction.WEST, aDoor);
+
+        return aMaze;
+    }
+}
